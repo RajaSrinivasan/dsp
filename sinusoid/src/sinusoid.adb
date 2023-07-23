56e01cd123afcde735f7010211227d0048e74f96
R Srinivasan <rs@toprllc.com>
2023-07-23 14:52:36 -0400

    pretty printed

--- sinusoid/src/sinusoid.adb
+++ sinusoid/src/sinusoid.adb
@@ -1,41 +1,36 @@
-with Ada.Numerics; use Ada.Numerics;
-with Ada.Command_Line; use Ada.Command_Line ;
-with Ada.Text_Io; use Ada.Text_IO;
+with Ada.Numerics;     use Ada.Numerics;
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_IO;      use Ada.Text_IO;
 
-with signal.sinusoid ;
-with wave ;
+with signal.sinusoid;
+with wave;
 
 procedure Sinusoid is
-   freq : float := 1.0 ;
-   amplitude : float := 1.0;
-   phase : float := Pi/2.0 ;
+   freq      : Float := 1.0;
+   amplitude : Float := 1.0;
+   phase     : Float := Pi / 2.0;
 
 begin
-   if Argument_Count < 1
-   then
-      Put_Line("usage: sinusoid outputfilename [<frequency> [<amplitude> [<phase>]]]");
+   if Argument_Count < 1 then
+      Put_Line
+        ("usage: sinusoid outputfilename [<frequency> [<amplitude> [<phase>]]]");
       return;
    end if;
-   if Argument_Count >= 2
-   then
-      freq := float'Value(Argument(2));
-      if Argument_Count >= 3
-      then
-         amplitude := float'Value(Argument(3));
-         if Argument_Count >= 4
-         then
-            phase := float'Value(Argument(4));
-         end if ;
+   if Argument_Count >= 2 then
+      freq := Float'Value (Argument (2));
+      if Argument_Count >= 3 then
+         amplitude := Float'Value (Argument (3));
+         if Argument_Count >= 4 then
+            phase := Float'Value (Argument (4));
+         end if;
       end if;
-   end if; 
+   end if;
    declare
-      sin : Signal.Sinusoid.Generator :=
-         ( frequency => freq ,
-            amplitude => amplitude ,
-            phase => phase ) ;
-      w : Wave.Wave_Type := Wave.Create(200,0.0,1.0);
+      sin : signal.sinusoid.Generator :=
+        (frequency => freq, amplitude => amplitude, phase => phase);
+      w : wave.Wave_Type := wave.Create (200, 0.0, 1.0);
    begin
-      Wave.Generate( w , sin ) ;
-      Wave.Print( w , Argument(1) );
-   end ;
-end Sinusoid ;
+      wave.Generate (w, sin);
+      wave.Print (w, Argument (1));
+   end;
+end Sinusoid;
