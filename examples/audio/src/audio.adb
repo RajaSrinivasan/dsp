14809080a80688b9d0111205b765b29b8800915c
R Srinivasan <rs@toprllc.com>
2023-07-31 05:42:07 -0400

    restore sources

---
+++ examples/audio/src/audio.adb
@@ -0,0 +1,13 @@
+with Ada.Numerics;     use Ada.Numerics;
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_IO;      use Ada.Text_IO;
+
+with signal.sinusoid;
+with wave;
+
+with sndfile;
+
+procedure Audio is
+begin
+   null;
+end Audio;
