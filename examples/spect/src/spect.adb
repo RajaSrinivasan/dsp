f880e5c3a29716fbf46f912a5bb254659cf6c80e
R Srinivasan <rs@toprllc.com>
2023-08-03 06:25:52 -0400

    clarifications

--- examples/spect/src/spect.adb
+++ examples/spect/src/spect.adb
@@ -1,32 +1,29 @@
 with Ada.Command_Line; use Ada.Command_Line;
 
-with Ada.Numerics ;
-with Ada.Command_Line ;
+with Ada.Numerics;
+with Ada.Command_Line;
 
-with Signal.Sinusoid ;
-with Signal.Square ;
-with signal.Noise ;
-with signal.sinc ;
-with signal.chirp ;
-with signal.test ;
+with signal.sinusoid;
+with signal.square;
+with signal.noise;
+with signal.sinc;
+with signal.chirp;
+with signal.test;
 
-with Wave ; use Wave ;
-with spectrum ;
-with windows ;
+with wave; use wave;
+with spectrum;
+with windows;
 
 procedure Spect is
-   sin : Signal.Sinusoid.Generator :=
-        ( frequency => 4.0 ,
-          amplitude => 2.0 ,
-          phase => Ada.Numerics.Pi/2.0 ) ;
-      w : Wave.Wave_Type := Wave.Create(200,0.0,0.5);
-      s : Spectrum.Freq_Type ;
-      wo : Wave.Wave_Type ;
-   begin
-      Wave.Generate( w , sin ) ;
-      s := Spectrum.Analyze(w) ;
-      Spectrum.Print(s,Argument(1) & ".csv");
-      wo := Spectrum.Synthesize(s) ;
-      Wave.Print(wo,Argument(1) & "_re.csv");
-   end Spect ;
-
+   sin : signal.sinusoid.Generator :=
+     (frequency => 4.0, amplitude => 2.0, phase => Ada.Numerics.Pi / 2.0);
+   w  : wave.Wave_Type := wave.Create (200, 0.0, 1.0);
+   s  : spectrum.Freq_Type;
+   wo : wave.Wave_Type;
+begin
+   wave.Generate (w, sin);
+   s := spectrum.Analyze (w);
+   spectrum.Print (s, Argument (1) & ".csv");
+   wo := spectrum.Synthesize (s);
+   wave.Print (wo, Argument (1) & "_re.csv");
+end Spect;
