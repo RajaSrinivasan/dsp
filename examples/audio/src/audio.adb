a5a75e6ef387d70e88338bacd873f814058a9766
R Srinivasan <rs@toprllc.com>
2023-08-13 02:19:01 -0400

    print version of lib

--- examples/audio/src/audio.adb
+++ examples/audio/src/audio.adb
@@ -1,13 +1,14 @@
 with Ada.Numerics;     use Ada.Numerics;
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Text_IO;      use Ada.Text_IO;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+--with Interfaces.C; use Interfaces.C;
 
-with signal.sinusoid;
 with wave;
 
 with sndfile;
 
 procedure Audio is
 begin
-   null;
+   Put_Line( Interfaces.C.Strings.Value(sndfile.Version) );
 end Audio;
