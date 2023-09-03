a858392565c653b401ffd815e5af10819c6ddf45
R Srinivasan <rs@toprllc.com>
2023-09-03 04:06:26 -0400

    build docs script

---
+++ examples/build_doc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R -e "tinytex::install_tinytex(force=TRUE)"
+
+pushd examples
+R -e "rmarkdown::render('sinusoid/doc/sinusoid.Rmd')"
+R -e "rmarkdown::render('chirp/doc/chirp.Rmd')"
+R -e "rmarkdown::render('triangle/doc/triangle.Rmd')"
+R -e "rmarkdown::render('arith/doc/arith.Rmd')"
+R -e "rmarkdown::render('freqd/doc/freqd.Rmd')"
+R -e "rmarkdown::render('spect/doc/spect.Rmd')"
+R -e "rmarkdown::render('audio/doc/audio.Rmd')"
+R -e "rmarkdown::render('stats/doc/stats.Rmd')"
+R -e "rmarkdown::render('synth/doc/synth.Rmd')"
+zip docs.zip */*/*.pdf */*/*.html
+popd
